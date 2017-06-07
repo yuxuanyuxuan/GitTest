2dac8244cdff3ed4cb021b3c9090fd4c6726e1f7
boyoo <1728262397@qq.com>
2017-06-07 18:03:09 +0800

    New Class

---
+++ app/src/main/java/com/example/yuxuan/gittest/NewActivity.java
@@ -0,0 +1,18 @@
+package com.example.yuxuan.gittest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yuxuan on 2017/6/7.
+ */
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
